6e44796137eddea510ab402eca4f75fed1e82b4e
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-05 17:16:45 +0530

    19429

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digit_6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+        
