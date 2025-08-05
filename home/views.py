3c66e283f7c4f92c7d2c3bb8e06d92b85e186a76
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-05 17:11:57 +0530

    19428

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+def home(request):
+    api_url = 'http://localhost:8000/api/menu/'
+    try:
+        response = requests.get(api_url)
+        menu_items = response.json()
+    except Exception as e:
+        menu_items = []
+    return render(request, 'home.html', {'menu_items': menu_items})
