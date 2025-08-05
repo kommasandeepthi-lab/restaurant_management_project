b4d193ae6ebdb1baf18849534a0a1b770965c0f1
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-05 17:36:17 +0530

    19430

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Menu, Order
 # Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description')
+    search_fields = ('name')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'total_amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('customer_username',)
+    filter_horizontal = ('order_items',)
