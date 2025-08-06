baa4fd22813caebf9498f7ce7f750355511de121
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-06 20:27:52 +0530

    22440

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage_view
+from .views import homepage_view, about_view
 
 urlpatterns = [
     path('', homepage_views, name='homepage'),
+    path('about/', about_view, name='about'),
 ]
