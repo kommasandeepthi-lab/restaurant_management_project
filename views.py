87db1ef78f50662b99db21f52b429fd0176550b9
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-11 14:25:50 +0530

    29901

--- views.py
+++ views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import MenuItem
 
-def home(request):
-    menu_items = MenuItem.objects.all()
-    return render(request, 'home.html', {'menu_items': menu_items})
+def homepage(request):
+    query = request.GET.get('q', '')
+    if query:
+        menu_items = MenuItem.objects.filter(name_icontains=query)
+    else:
+        menu_items = MenuItem.objects.all()
+
+    return render(request, 'home.html', {
+        'menu_items': menu_items,
+        'query': query
+    })
