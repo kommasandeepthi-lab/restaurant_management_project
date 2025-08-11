f826baf0d5c5a2f01903ab23ffd88a5f6c0692a6
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-11 13:55:56 +0530

    29135

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import contact_view
 
 urlpatterns = [
     path('', views.home, name='home')
     path('menu/', views.menu, name='menu'),
+    path('contact/', contact_view, name='contact'),
 ]
