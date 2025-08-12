5d731fc831044802d2e4f673c62c67e261b11f2b
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-12 11:57:36 +0530

    29902

--- views.py
+++ views.py
@@ -1,14 +1,3 @@
-from django.shortcuts import render
-from .models import MenuItem
-
-def homepage(request):
-    query = request.GET.get('q', '')
-    if query:
-        menu_items = MenuItem.objects.filter(name_icontains=query)
-    else:
-        menu_items = MenuItem.objects.all()
-
-    return render(request, 'home.html', {
-        'menu_items': menu_items,
-        'query': query
-    })
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from .form
