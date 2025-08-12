214273358c748c6782cc584fb0510fbd6b324d84
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-12 13:32:34 +0530

    29903

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django conf import settings
 
 urlpatterns = [
     path('', include('your_app_name.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
