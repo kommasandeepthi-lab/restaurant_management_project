7f8d206fd4524ad1c959b4728529e60bcc2f293b
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-12 13:35:46 +0530

    29903

--- views.py
+++ views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail
-from .form
+from django.shortcuts import render
+from .models import MenuItem
+
+def menu_list(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'menu_list.html', {'menu_items': menu_items})
