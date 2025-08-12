e5ed4b8099bf69aba8f9f83d8d51534310a4c393
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-12 12:19:48 +0530

    29902

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -133,11 +133,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'OurRestaurant@gmail.com'
-EMAIL_HOST_PASSWORD = 12345678
-DEAULT_FROM_EMAIL = EMAIL_HOST_USER
