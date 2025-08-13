f8c5a3cf49549587daaf216af0592b98b9c62c4b
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-13 12:35:14 +0530

    31827

--- views.py
+++ views.py
@@ -4,3 +4,6 @@ from .models import Restaurant
 def homepage(request):
     restaurant = Restaurant.objects.first()
     return render(request, 'index.html', {'restaurant': restaurant})
+    
+    current_datetime = timezone.now()
+    return render(request, 'index.html', {'current_datetime': current_datetime})
