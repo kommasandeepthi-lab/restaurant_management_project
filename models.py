95c9544aa94a0994a64dfb6db1b369b18e193b7f
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-13 12:17:42 +0530

    31118

--- models.py
+++ models.py
@@ -1,13 +1,10 @@
 from django.db import models
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=150)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-    address = models.TextField()
-    phone_number = models.CharField(max_length=20, blank=True)
-    opening_hours = models.JSONField(default=dict, blank=True)
+class Restaurant(models.Model)
+    name = models.CharField(max_length=100)
+    history = models.TextField()
+    mission = models.TextField()
+    logo = models.ImageField(upload_to='restaurant_logos/')
 
     def __str__(self):
         return self.name
