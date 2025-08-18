2f057c9f68602ebec7490962cbe7764e8c7e0644
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-18 18:03:16 +0530

    34579

--- admin.py
+++ admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import MenuItem
+from .models import TodaySpecial
 
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+admin.site.register(TodaySpecial)
