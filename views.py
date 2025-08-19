4620e6d4727ff21cb6f42c4f90ae8dca5d099ae0
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-19 15:13:46 +0530

    35302

--- views.py
+++ views.py
@@ -1,23 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from .models import RestaurantAddress
 
 def contact_view(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
+    address = RestaurantAddress.objects.first()
 
-            print("Message received:", name, email, message)
-
-            return render(request, "contact_success.html", {"name": name})
-
-            return redirect("thank you")
+    if request.method == "POST";
+       form = ContactForm(request.POST)
+       if form.is_valid():
+        return redirect("thank_you")
     else:
         form = ContactForm()
-    
-    return render(request, "contact.html", {"form": form})
 
-def thank_you_view(request):
-    return render(request, "thank_you.html")
+    return render(request, "contact.html", {"form": form, "address": address})
