24363c321653db2a1fec6065a6aa8826376a5a24
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-19 14:52:50 +0530

    35304

--- forms.py
+++ forms.py
@@ -9,3 +9,15 @@ class FeedbackForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
             'feedback_text': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Write your feedback here...'}),
         }
+
+class ContactForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your full name'})
+    )
+    email = forms.EmailFiels(
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email address'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Write your message here...', 'rows': 5})
+    )
