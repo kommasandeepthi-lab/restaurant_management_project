1564fc453283af18e616cc7c46f832e965e9c43f
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-20 13:06:36 +0530

    35806

--- account/models.py
+++ account/models.py
@@ -12,10 +12,10 @@ class UserProfile(models.Model):
         return self.user.username
 
 class TodaySpecial(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.ImageField(upload_to="menu_images/", blank=True, null=True)
 
 
     def __str__(self):
