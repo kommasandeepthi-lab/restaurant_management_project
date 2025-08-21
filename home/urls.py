1233ef0da7fdf72b073c062f5c99e6d7dbf01f6e
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-21 12:59:27 +0530

    36446

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('', views.homepage, name='home'),
     path)"menu/", views.menu_view, name="menu"),
+    path("about/", views.about, name="about"),
 ]
