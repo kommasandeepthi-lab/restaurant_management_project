6bddc7d7beb357647a0aaeb4c28166ccc2b4069d
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-08-26 18:58:26 +0530

    38889

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django conf import settings
 from . import views
-
+from .views import contact_view, contact_success_view
 urlpatterns = [
     path('', include('your_app_name.urls')),
     path('', views.homepage, name='homepage'),
     path("about/", views.about_view, name="about"),
+    path("contact/", contact_view, name="contact"),
+    path("contact/success/", contact_success_view, name="contact_success"),
 ]
 
 if settings.DEBUG:
