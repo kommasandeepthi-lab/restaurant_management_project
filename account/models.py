ef71d3a0286989ee24ff35a6889af86e5edc47a8
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-02 13:34:44 +0530

    41508

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -42,4 +43,9 @@ address = models.CharField(max_length=255)
 
     def __str__(self):
         return self.address
-        
+
+class Loction(models.Model)
+address = model.CharField(max_length=100)
+
+    def __str__(self):
+        return self.address
