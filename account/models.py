8e7d25da7cdf362ee7cdf66005d40f4bcd3dcbe0
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-09 12:05:43 +0530

    43414

--- account/models.py
+++ account/models.py
@@ -64,3 +64,17 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_total(self):
+        return self.product.price * self.quantity
