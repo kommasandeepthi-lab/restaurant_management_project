a645ca7483e948b41a01d352bd3a6858101ff0e2
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-17 13:05:23 +0530

    46679

--- views.py
+++ views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .forms import ContactForm
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-def Contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Thank you, your message has been sent!")
-            return redirect("contact")
-    else:
-        form = ContactForm()
+class MenuItemViewSet(viewsets, ViewSet):
+    permission_classes = [IsAdminUser]
 
-    return render(request, "contact.html", {"form": form})
+    def update(self, request, pk=None):
+        try:
+            item = MenuItem.objects.get(pk=pk)
+        except MenuItem.DoesNotExist:
+            return Response({"error": "Menu Item Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MenuItemSerializer(item, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
