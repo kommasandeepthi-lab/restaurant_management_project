ac1b052f38c04943ec1865c255b7f839c48cf76f
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-17 11:18:07 +0530

    46681

--- account/models.py
+++ account/models.py
@@ -118,3 +118,19 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.product_name} - {self.quantity}"
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name="items")
+
+    def __str__(self):
+        return self.name
+
