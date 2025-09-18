0b4b3fce8b004c021c76810a8c676dc9a1e0ae64
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-18 14:57:25 +0530

    47561

--- serializer.py
+++ serializer.py
@@ -1,7 +1,16 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializer
+from .models import Order
+from home.models import MenuItem
 
-class UserUpdateSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializer.ModelSerializer):
     class Meta:
-        model = User
-        fields = ["first_name", "last_name", "email"]
+        model = MenuItem
+        fields = ["id", "name", "price"]
+
+class OrderSerializer(serializer.ModelSerializer):
+    items = MenuItemSerializer(many=True, read_only=True)
+    user = serializer.StringRelatedField()
+
+    class Meta:
+        model = Order
+        fields = ["order_id", "user", "items", "total_price", "created_at"]
