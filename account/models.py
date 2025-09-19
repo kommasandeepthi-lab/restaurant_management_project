fd7b710aedef6f428e0a573b747a43c1d966b1e9
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-19 13:07:11 +0530

    48522

--- account/models.py
+++ account/models.py
@@ -174,3 +174,12 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
+class ContactFormSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"
+
