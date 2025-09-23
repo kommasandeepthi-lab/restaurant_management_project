1f5eda4a8c178ab235cedfbc219e8e066ce4b003
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-23 13:16:47 +0530

    50260

--- serializers.py
+++ serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from .models import MenuItem
+from.models import Order
 
-class DailySpecialSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuItem
-        fields = ['id', 'name', 'description', 'price']
+class OrderStatusUpdateSerializer(serializer.Serializer):
+    order_id = serializers.IntegerField()
+    new_status = serializers.ChoiceField(choices=Order.STATUS_CHOICES)
