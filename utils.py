75fa595d123b737432d34fd8b145c43d74ded45b
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-26 14:55:54 +0530

    51239

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ def is_valid_email(email: str) -> bool:
     if not isinstance(email, str):
         return False
 
-    pattern = r[a-zA-z09_]
+    pattern = r'[a-zA-z0-9_.+-]+@[a-zA-z0-9-]'
     return bool(re,match(pattern, email))
