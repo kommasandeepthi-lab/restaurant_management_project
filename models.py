51f05985d425236c527a2a809cc9919e448f1adc
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-26 15:03:07 +0530

    51238

--- models.py
+++ models.py
@@ -1,36 +1,18 @@
 from django.db import models
-from django.utils import timezone
-from datetime import timedelta
 
-class Reservation(models.Model):
-    customer_name = models.CharField(max_length=255)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_percentage = models.DecimalField(
+        max_digits=5, decimal_places=2, default=0.0
+        help_text="Discount percentageto apply to the item price"
+    )
     def __str__(self):
-        return f"Reservation for {self.customer_name} from {self.start_time} to {self.end_time}"
+        return self.name
+    
+    def final_price(self) -> float:
 
-    @classmethod
-    def get_available_slots(cls, start_range, end_range, slot_length=60):
-        existing_reservatioms = cls.objects.filter(
-            start_time__lt=end_range,
-            end_time__gt=start_range
-        ).order_by('start_time')
-
-        available_slots = []
-        current_start = start_range
-
-        while current_start + timedelta(minutes=slot_length) <= end_range:
-            slot_end = current_start + timedelta(minutes=slot_length)
-
-            overlap = existing_reservatioms.filter(
-                start_time__lt=slot_end,
-                end_time__gt=current_start
-            ).exists()
-
-            if not overlap:
-                available_slots.append((current_start, slot_end))
-
-            current_start = slot_end
-
-        return available_slots
+        if self.discount_percentage > 0:
+            discount_amount = (self.discount_percentage / 100) * float(self.price)
+            return round(float(self.price) - discount_amount, 2)
+        return float(self.price)
