b7e0fea503d6a7964c5fabdb1fc220f9805b9250
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-09-26 15:19:03 +0530

    51236

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
-import re
-def is_valid_email(email: str) -> bool:
+def calculate_discount(original_price, discount_percentage):
+    try:
+        original_price = float(original_price)
+        discount_percentage = float(discount_percentage)
 
-    if not isinstance(email, str):
-        return False
+        if original_price < 0:
+            raise ValueError("Original price cannot be negative.")
+        if not(0 <= discount_percentage <= 100):
+            raise ValueError("Discount percentage must be between 0 and 100.")
 
-    pattern = r'[a-zA-z0-9_.+-]+@[a-zA-z0-9-]'
-    return bool(re,match(pattern, email))
+        discount_amount = (discount_percentage / 100) * original_price
+        discount_price = original_price - discount_amount
+
+        return round(discounted_price, 2)
+
+    except (ValueError, TypeError) as e:
+        print(f"Error calculating discount: {e}")
+        return None
