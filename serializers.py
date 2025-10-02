9fa0e4dfb54094cc1dbff5cb6391cbdffcbc3d40
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-02 11:22:26 +0530

    52847

--- serializers.py
+++ serializers.py
@@ -1,9 +1,12 @@
-from rest_framework import serializers
-from .models import MenuItem
+from rest_framework import serializer
+from .models import Restaurant
 
-class MenuItemSerializer(serializers.ModelSerializer):
-    category = serializers.StringRelatedField()
+class RestaurantHoursSerializer(serializers.ModelSerializer):
+    opening_hours = serializer.SerializerMethodField()
 
     class Meta:
-        model = MenuItem
-        fields = ["id", "name", "description", "price", "category"]
+        model = Restaurant
+        fields = ["opening_hours"]
+
+    def get_opening_hours(self, obj):
+        return f"{obj.opening_time.strftime('%I:%M %p')} - {obj.closing_time.strftime('%I:%M %p)} ({obj.timezone})"
