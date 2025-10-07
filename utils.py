2d1db44378f5d68599d99562ead3841ec97f357d
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-07 14:09:42 +0530

    53809

--- utils.py
+++ utils.py
@@ -1,11 +1,27 @@
-def calculate_average_rating(reviews_queryset):
+import re
+
+def format_phone_number(phone_number: str) -> str:
+    
     try:
-        total_reviews = reviews_queryset.count()
-        if total_reviews == 0:
-            return 0.0
+        if not isinstance(phone_number, str):
+            raise ValueError("Phone number must be a strong.")
 
-        total_rating = sum(review.rating for review in reviews_queryset)
-        return round(total_rating / total_reviews, 2)
+        digits = re.sub(r'\D', '', phone_number)
 
-    except Exception:
-        return 0.0
+        if not digits:
+            raise ValueError("Phone number contains no digits.")
+
+        if len(digits) == 10:
+            formatted = f"({digits[:3]}) {digits[3:6]}-{digits[6:]}"
+        elif len(digits) == 11 and digits.startswith('1'):
+
+            formatted = f"+1 ( ({digits[1:4]}) {digits[4:7]}-{digits[7:]}"
+        elif len(digits) == 12 and digits.startswith('91'):
+            formatted = f"+91 {digits[2:7]}-{digits[7:]}"
+        else:
+            formatted = f"+{digits}"
+
+        return formatted
+
+    except Exception as e:
+        return f"Invalid phone number: {str(e)}"
