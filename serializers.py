c2282ce6e43227f6930d22503f259614ba960bab
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-07 14:01:44 +0530

    53810

--- serializers.py
+++ serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from .models import Order
+from .models import FAQ
 
-class OrderHistorySerializer(serializers.ModelSerializer):
+class FAQSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Order
-        fields = ['id', 'date', 'items', 'total_amount', 'status']
+        model = FAQ
+        fields = ['id', 'question', 'answer']
