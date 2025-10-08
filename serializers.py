7e0682b16fffe714564455cb414a3cc2764d5dd7
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-08 12:22:05 +0530

    54060

--- serializers.py
+++ serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, OrderItem
 
-class OrderStatusUpdateSerializer(serializers.ModelSerializer):
+class OrderItemSerializer(serializers.ModelSerializer):
+    menu_item_name = serializers.CharField(source='menu_item.name', read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['menu_item_name', 'quantity', 'quantity', 'price']
+
+class OrderSummarySerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
     class Meta:
         model = Order
-        fields = ['status']
-
-    def validate_status(self, value):
-        valid_statuses = [choice[0] for choice in Order.STATUS_CHOICES]
-        if value not in valid_statuses:
-            raise serializers.ValidationError(f"Invalid status. Choose from {valid_statuses}.")
-        return value
+        fileds = ['id', 'total_price', 'status', 'items']
