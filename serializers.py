8d8943f4ccd7eccd54de9a897d9e26fe425a31bc
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-08 12:31:23 +0530

    54206

--- serializers.py
+++ serializers.py
@@ -1,16 +1,9 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
+from .models import Review
 
-class OrderItemSerializer(serializers.ModelSerializer):
-    menu_item_name = serializers.CharField(source='menu_item.name', read_only=True)
-
-    class Meta:
-        model = OrderItem
-        fields = ['menu_item_name', 'quantity', 'quantity', 'price']
-
-class OrderSummarySerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
+class ReviewSerializers(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
-        model = Order
-        fileds = ['id', 'total_price', 'status', 'items']
+        model = Review
+        fields = ['user_name', 'rating', 'comment']
