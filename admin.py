a62e1670e94489aa4f51ee9ca06e02a69671fcfa
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-10 11:56:59 +0530

    54379

--- admin.py
+++ admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Table
+from .models import MenuCategory
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     list_display = ('id', 'table_number', 'capacity', 'is_available')
     search_fields = ('table_number',)
     list_filter = ('is_available',)
+
+admin.site.register(MenuCategory)
