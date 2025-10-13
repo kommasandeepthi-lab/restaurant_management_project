149caa9bb50a3619b300b0c4eeeb0e81a469b639
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-13 10:38:04 +0530

    54570

--- utils.py
+++ utils.py
@@ -1,25 +1,4 @@
-from datetime import datetime, time
+from .models import Table
 
-def is_restaurant_open():
-
-    working_hours = {
-        0: (time(5, 0), time(22, 0)),
-        1: (time(5, 0), time(22, 0)),
-        2: (time(5, 0), time(22, 0)),
-        3: (time(5, 0), time(22, 0)),
-        4: (time(5, 0), time(22, 0)),
-        5: (time(8, 0), time(23, 0)),
-        6: (time(8, 0), time(20, 0)),
-    }
-
-    now = datetime.now()
-    current_day = now.weekday()
-    current_time = now.time()
-
-    if current_day in working_hours:
-        open_time, close_time = working_hours[current_day]
-
-        if open_time < = current_time <= close_time:
-            return True
-
-    return False
+def get_available_tables_by_capacity(num_guests):
+    return Table.objects.filter(is_available=True, capacity__gte=num_guests)
