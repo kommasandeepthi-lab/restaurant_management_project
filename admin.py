1f2508c76178840a5505a370b5f8079be40c1e58
kommasandeepthi-lab <kommasandeepthi@gmail.com>
2025-10-13 10:40:52 +0530

    54955

--- admin.py
+++ admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Table
 from .models import MenuCategory
+from .models import Restaurant
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
     list_display = ('table_number', 'capacity', 'is_available', 'location')
     search_fields = ('table_number', 'location')
     list_filter = ('is_available', 'location')
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
